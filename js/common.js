ba4a70c47755d98c2ffe5dd5f5f9ea225081a5dd
yong.liu1 <yong.liu1@dmall.com>
2022-09-07 13:14:58 +0800

    update

--- js/common.js
+++ js/common.js
@@ -14,4 +14,16 @@ $(function () {
       $('body,html').animate({scrollTop:0},1000);
       return false;
   });
+
+  
+  if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))){
+    var wow = new WOW({
+      boxClass: 'wow', // ‘wow’需要执行动画的元素的 class
+      animateClass: 'animated', // ‘animated’animation.css 动画的 class
+      offset: 0, // 距离可视区域多少开始执行动画
+      mobile: true, // 是否在移动设备上执行动画
+      live: true // 异步加载的内容是否有效
+    });
+    wow.init();
+  };
 });
